0cf5dc26804d687e4faf5732daceaeda6de52ed0
BilyalovE <eldar2117@gmail.com>
2024-09-27 03:01:03 +0300

    Task 1 week4

--- Week 4/Solve tasks/Task 1/Task 1/main.cpp
+++ Week 4/Solve tasks/Task 1/Task 1/main.cpp
@@ -6,9 +6,44 @@
 //
 
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+using namespace std;
+
+void PrintVectorPart(const vector<int>& numbers){
+    auto it = find_if(begin(numbers),
+    end(numbers),
+    [](const auto i){
+    return i < 0;
+    });
+    if(it != end(numbers)){
+        while (it != begin(numbers)){
+            --it;
+            if (it != begin(numbers)){
+                cout << *it << ", ";
+            }
+            else{
+                cout << *it << endl;
+            }
+        }
+    }
+    else if (it == end(numbers)){
+        while ( --it != prev(begin(numbers))){
+            if (it != begin(numbers)){
+                cout << *it << ", ";
+            }
+            else{
+                cout << *it << endl;
+            }
+        }
+    }
+}
+
+
+int main() {
+  PrintVectorPart({6, 1, 8, -5, 4});
+  PrintVectorPart({-6, 1, 8, -5, 4});  // ничего не выведется
+  PrintVectorPart({6, 1, 8, 5, 4});
+  return 0;
 }
